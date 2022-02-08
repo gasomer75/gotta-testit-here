d1c12f0b9e14e036b7d708d0a8b09356b7c2a4a8
gsomer <gasomer75@hotmail.com>
2022-02-08 10:17:31 -0800

    yelp test update-4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,15 @@
-var restaurantList = document.querySelector(".rest-ul");
-function getApi() {
-    var access_token = "Bearer _l5f7C7BDfq3ksCHSP4VGgHaTrUQoYkCxJRAKg1Um2gqJyxYHChBz_dB-vR4Gvlrzk00YQcsfqHemlq0aJ0EWtMJ5dEDoh436BSFUkpn-bZlV1ABayxGf1K3n-b9YXYx";
-    var url = 'https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=37.6849084&longitude=-120.9611351?Authorization=' + access_token;
-  
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Access-Control-Allow-Origin': "https://gasomer75.github.io/gotta-testit-here/",
-            'Accept': 'application/json',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Cache-Control': 'no-cache'
-        },
-        mode: 'no-cors'
-    })
-    .then(function(response) {
-    console.log(response.json);
-    return response.json();
-    })
-    .then(function(data) {
-    for (var i = 0; i < 5; i++) {
-        var listItem = document.createElement('li');
-        console.log(data[i]);
-    
-        listItem.textContent = data[i];
 
-        restaurantList.appendChild(listItem);
-    }
-    })
-};
+
+const access_token = "_l5f7C7BDfq3ksCHSP4VGgHaTrUQoYkCxJRAKg1Um2gqJyxYHChBz_dB-vR4Gvlrzk00YQcsfqHemlq0aJ0EWtMJ5dEDoh436BSFUkpn-bZlV1ABayxGf1K3n-b9YXYx";
+
+let myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer " + access_token);
+
+fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=bars&limit=50&location=New York", {
+  headers: myHeaders 
+}).then((res) => {
+  return res.json();
+}).then((json) => {
+  console.log(json);
+});
 getApi();
