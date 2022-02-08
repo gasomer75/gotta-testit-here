518a04d1f6ecd21316652ef8be7f46ba075d59e3
gsomer <gasomer75@hotmail.com>
2022-02-08 02:18:55 -0800

    yelp test update-1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
-
+var restaurantList = document.querySelector(".rest-ul");
 function getApi() {
     var url = 'https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=37.6849084&longitude=-120.9611351';
   
-    var myInit = { method: 'GET',
-                  headers: {
-                      'Access-Control-Allow-Origin': 'https://gasomer75.github.io/gotta-testit-here/',
-                      'Authorization': 'Bearer _l5f7C7BDfq3ksCHSP4VGgHaTrUQoYkCxJRAKg1Um2gqJyxYHChBz_dB-vR4Gvlrzk00YQcsfqHemlq0aJ0EWtMJ5dEDoh436BSFUkpn-bZlV1ABayxGf1K3n-b9YXYx',
-                      'Accept': 'application/json',
-                      'Accept-Encoding': 'gzip, deflate, br',
-                      'Connection': 'keep-alive',
-                      'Cache-Control': 'no-cache'
-                    },
-                  mode: 'no-cors',
-                  };
-  
-    var myRequest = new Request(url);
-  
-    fetch(myRequest, myInit)
-          .then(function(response) {
-          console.log(response);
-        });
-  }
-  getApi();
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Access-Control-Allow-Origin': "https://gasomer75.github.io/gotta-testit-here/",
+            'Authorization': "Bearer _l5f7C7BDfq3ksCHSP4VGgHaTrUQoYkCxJRAKg1Um2gqJyxYHChBz_dB-vR4Gvlrzk00YQcsfqHemlq0aJ0EWtMJ5dEDoh436BSFUkpn-bZlV1ABayxGf1K3n-b9YXYx",
+            'Accept': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache'
+        },
+        mode: 'no-cors'
+    })
+    .then(function(response) {
+    console.log(response.json);
+    return response.json();
+    })
+    .then(function(data) {
+    for (var i = 0; i < 5; i++) {
+        var listItem = document.createElement('li');
+        console.log(data[i]);
+    
+        listItem.textContent = data[i];
+
+        restaurantList.appendChild(listItem);
+    }
+    })
+}
+getApi();
