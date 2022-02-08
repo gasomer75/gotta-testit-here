bbe9b0643a353de60bd53f308c36c15fc40b7b96
gsomer <gasomer75@hotmail.com>
2022-02-08 10:26:04 -0800

    yelp test update-4.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-
+// FINALLY - Working fetch for Yelp's API - do not change (except url for variable addition)
 const access_token = "_l5f7C7BDfq3ksCHSP4VGgHaTrUQoYkCxJRAKg1Um2gqJyxYHChBz_dB-vR4Gvlrzk00YQcsfqHemlq0aJ0EWtMJ5dEDoh436BSFUkpn-bZlV1ABayxGf1K3n-b9YXYx";
 
 let myHeaders = new Headers();
@@ -12,3 +12,4 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/se
 }).then((json) => {
   console.log(json);
 });
+// End of working fetch statement - do not change
